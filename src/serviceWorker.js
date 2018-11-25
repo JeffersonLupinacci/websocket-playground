86ca10a73c1d0ca18bf82a826ba17865699baed9
Jefferson Lupinacci <jeffersonlupinacci@outlook.com>
2018-11-25 18:21:50 +0100

    FetchApi

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -43,6 +43,22 @@ const isLocalhost = Boolean(
         registerValidSW(swUrl);
       }
     });
+
+    window.addEventListener('fetch', function(event) {
+      event.respondWith(
+        caches.match(event.request).then(function(resp) {
+          return resp || fetch(event.request).then(function(response) {
+            caches.open('v1').then(function(cache) {
+              cache.put(event.request, response.clone());
+            });
+            return response;
+          });
+        }).catch(function(error) {
+          console.log(error);
+        })
+      );
+    });
+
   }
  }
  
